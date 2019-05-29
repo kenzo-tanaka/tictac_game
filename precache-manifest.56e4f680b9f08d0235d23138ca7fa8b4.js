9073a6385e068e8d0f64a69d2b2a0790d3394c78
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-05-29 10:15:58 +0900

    Updates

---
+++ precache-manifest.56e4f680b9f08d0235d23138ca7fa8b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67cab1244ae8c4de88f252c9925e5d57",
+    "url": "/tictac_game/index.html"
+  },
+  {
+    "revision": "418bb6a05d99abf33654",
+    "url": "/tictac_game/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tictac_game/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "418bb6a05d99abf33654",
+    "url": "/tictac_game/static/js/main.04005dec.chunk.js"
+  },
+  {
+    "revision": "3c3aa28e9484743d0ba2",
+    "url": "/tictac_game/static/js/runtime~main.0e1f545d.js"
+  }
+]);
